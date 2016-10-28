42f9d15f8991ba88d29e08def3c938001b2b760c
Kacper Łukawski <ka.lukawski@gmail.com>
2016-10-28 22:57:13 +0200

    cs7 in progress,  halfway done

--- paiJS/cw7/js/main.js
+++ paiJS/cw7/js/main.js
@@ -1,4 +1,18 @@
 var clickCount = 0
+
 function pierwszyKlik () {
-    alert('Kliknięcie numer ' + clickCount++ + '!')
+  alert('Kliknięcie numer ' + clickCount++ + '!')
+}
+
+function napis (imie, nazwisko, wiek) {
+  var napis = 'Nazywasz się ' + imie + ' ' + nazwisko + ' i masz ' + wiek + ' lat.'
+  return napis
+}
+
+function reloadPage () {
+  location.reload()
+}
+
+function changeBg () {
+  document.body.style.backgroundColor = 'green'
 }
